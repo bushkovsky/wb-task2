6222efbde6f33626e20669c558a3964b8418bc9a
bushkovsky <62966446+bushkovsky@users.noreply.github.com>
2025-01-07 18:56:19 +0300

    5

--- 5/main.go
+++ 5/main.go
@@ -1,5 +1,33 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+var n int
+var wg sync.WaitGroup
+
+func printSecond(ch chan int) {
+	defer wg.Done()
+
+	for i := range ch {
+		fmt.Println(i)
+	}
+}
 func main() {
+	n = 5
+	ch := make(chan int)
+	wg.Add(1)
+	go printSecond(ch)
+
+	for i := 0; i < n; i++ {
+		time.Sleep(time.Second)
+		ch <- i
+	}
+
+	close(ch)
+	wg.Wait()
 
 }
